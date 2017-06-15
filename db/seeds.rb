1610661d0b34c3adecfea17453fb0e135164becc
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 14:47:39 +0000

    Added basic portfolio feature and fixed swapped images in the seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,8 @@ puts "Created five skills"
     aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
     irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "”http://placehold.it/350x150”" , 
-    thumb_image: "http://placehold.it/600x400"
+    main_image: "http://placehold.it/600x400" , 
+    thumb_image: "http://placehold.it/350x150"
     )
 end
+puts "Created 9 portfolio items"
