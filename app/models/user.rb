e98d0622da1c93442e10f5b906fef9a7a7f81da7
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 15:28:03 +0000

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  validates_presence_of :name
+  
+  def first_name
+    name = self.name.split
+    name.delete_at(-1) if name.length > 1
+    name.join(" ")
+  end
+  def last_name
+    self.name.split.last
+  end
 end
